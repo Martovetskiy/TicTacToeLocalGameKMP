154c510a3f0ba82f8cbb532f209ac792e608d709
Stepan <stepan.bodnar43@gmail.com>
2024-08-04 01:50:18 +0700

    feat: Change Themes Concept

--- composeApp/src/desktopMain/kotlin/ui/Colors.kt
+++ composeApp/src/desktopMain/kotlin/ui/Colors.kt
@@ -1,79 +1,50 @@
 package ui
 
-import androidx.compose.ui.SystemTheme
 import androidx.compose.ui.graphics.Color
 
-class DefaultTheme {
-    val Accent = Color(0xFF4E35F2)
 
-    val Dark = Color(0xFF262626)
-    val Light = Color(0xFFFFFFFF)
+data class Theme(
+    val background: Color,
+    val card: Color,
+    val text: Color,
+    val accent: Color,
 
-    val Gray = Color(0xFF787878)
-    val Gray2 = Color(0xFF969696)
-    val Gray3 = Color(0xFFE4E4E4)
-    val Red = Color(0xFFF23535)
+    val name: String = "Undefined",
+    )
 
-    val Gray4 = Color(0xFFF5F5F5)
+//val Dark = Color(0xFF262626)
+val Light = Color(0xFFFFFFFF)
 
-    val BackgroundColor = Gray4
-    val CardColor = Light
-    val TextColor = Dark
+val DarkPurple = Color(0xFF522B5B)
+val SkyColor = Color(0xFF2B124C)
 
-}
+val LightPurple = Color(0xFFFAF4FB)
+val Carbon = Color(0xFF251D3E)
+//val Purple  = Color(0xFFB379CA)
 
-class BloodAndWaterTheme {
+val DarknessGreen = Color(0xFF117C6F)
+val DarkGreen =  Color(0xFF11533E)
+//val Green = Color(0xFF289CBE)
+//val SoftGreen = Color(0xFF2FC4B2)
+val LightGreen = Color(0xFF8DE5DB)
+//val WhiteGreen = Color(0xFFB2F0E8)
 
-    val Dark = Color(0xFF262626)
-    val Light = Color(0xFFFFFFFF)
-
-    val Gray = Color(0xFF787878)
-    val Gray2 = Color(0xFF969696)
-    val Gray3 = Color(0xFFE4E4E4)
-    val Red = Color(0xFFF23535)
-
-    val Gray4 = Color(0xFFF5F5F5)
-
-    val Accent = Color(0xFFDF3A31)
-
-    val BackgroundColor = Color(0xFF2A4871)
-    val CardColor = Color(0xFF718BAE)
-    val TextColor = Color(0xFFD9EDEC)
-
-}
+val Morena = Color(0xFF1F7062)
 
 
-class PurpleTheme {
+val PurpleTheme = Theme(
+    background = DarkPurple,
+    card = LightPurple,
+    text = SkyColor,
+    accent = Carbon,
+    name = "Киберпанк"
+)
 
-    val Dark = Color(0xFF262626)
-    val Light = Color(0xFFFFFFFF)
-
-    // my colors
-    val DarkPurple = Color(0xFF522B5B)
-    val SkyColor = Color(0xFF2B124C)
-
-    val Purple  = Color(0xFFB379CA)
-    val LightPurple = Color(0xFFFAF4FB)
-    val Carbon = Color(0xFF251D3E)
-
-    val BackgroundColor = DarkPurple
-    val CardColor = LightPurple
-    val TextColor = SkyColor
-    val CoolColor = Color(0xFF33fff9)
-    val Accent = Carbon
-}
-
-class GreenTheme{
-    val Dark = Color(0xFF262626)
-    val Light = Color(0xFFFFFFFF)
-    val DarkGreen = Color(0xFF117C6F)
-    val Green = Color(0xFF289CBE)
-    val SoftGreen = Color(0xFF2FC4B2)
-    val LightGreen = Color(0xFF8DE5DB)
-    val WhiteGreen = Color(0xFFB2F0E8)
-    val BackgroundColor = DarkGreen
-    val CardColor = LightGreen
-    val TextColor = Color(0xFF11533E)
-    val Accent = Color(0xFF1F7062)
-}
+val GreenTheme =  Theme(
+    background = DarknessGreen,
+    card = LightGreen,
+    text = DarkGreen,
+    accent = Morena,
+    name = "Летняя"
+)
 
