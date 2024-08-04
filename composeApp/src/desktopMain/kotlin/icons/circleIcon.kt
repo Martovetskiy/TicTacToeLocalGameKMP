8e5a786bd453a5123cee4ff5e1b96c446e3764f1
Stepan <stepan.bodnar43@gmail.com>
2024-08-04 16:06:37 +0700

    feat: Add Game icons

---
+++ composeApp/src/desktopMain/kotlin/icons/circleIcon.kt
@@ -0,0 +1,43 @@
+package icons
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import kotlin.Suppress
+
+val Circle: ImageVector
+    get() {
+        if (_Circle != null) {
+            return _Circle!!
+        }
+        _Circle = ImageVector.Builder(
+            name = "Circle",
+            defaultWidth = 140.dp,
+            defaultHeight = 140.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round
+            ) {
+                moveTo(21f, 12f)
+                curveTo(21f, 16.971f, 16.971f, 21f, 12f, 21f)
+                curveTo(7.029f, 21f, 3f, 16.971f, 3f, 12f)
+                curveTo(3f, 7.029f, 7.029f, 3f, 12f, 3f)
+                curveTo(16.971f, 3f, 21f, 7.029f, 21f, 12f)
+                close()
+            }
+        }.build()
+
+        return _Circle!!
+    }
+
+@Suppress("ObjectPropertyName")
+private var _Circle: ImageVector? = null
---
+++ composeApp/src/desktopMain/kotlin/icons/crossIcon.kt
@@ -0,0 +1,53 @@
+package icons
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import kotlin.Suppress
+
+val Cross: ImageVector
+    get() {
+        if (_Cross != null) {
+            return _Cross!!
+        }
+        _Cross = ImageVector.Builder(
+            name = "Cross",
+            defaultWidth = 140.dp,
+            defaultHeight = 140.dp,
+            viewportWidth = 140f,
+            viewportHeight = 140f
+        ).apply {
+            path(fill = SolidColor(Color(0xFF000000))) {
+                moveTo(140f, 8.67f)
+                curveTo(140f, 6.33f, 139.09f, 4.13f, 137.44f, 2.48f)
+                curveTo(134.13f, -0.83f, 128.37f, -0.83f, 125.06f, 2.48f)
+                lineTo(70f, 57.58f)
+                lineTo(14.94f, 2.48f)
+                curveTo(11.63f, -0.83f, 5.87f, -0.83f, 2.56f, 2.48f)
+                curveTo(0.91f, 4.13f, 0f, 6.33f, 0f, 8.67f)
+                curveTo(0f, 11.01f, 0.91f, 13.21f, 2.56f, 14.86f)
+                lineTo(57.63f, 69.96f)
+                lineTo(2.56f, 125.06f)
+                curveTo(0.91f, 126.71f, 0f, 128.91f, 0f, 131.24f)
+                curveTo(0f, 133.58f, 0.91f, 135.78f, 2.56f, 137.43f)
+                curveTo(5.87f, 140.74f, 11.63f, 140.74f, 14.94f, 137.43f)
+                lineTo(70f, 82.34f)
+                lineTo(125.06f, 137.43f)
+                curveTo(126.72f, 139.09f, 128.91f, 140f, 131.25f, 140f)
+                curveTo(133.59f, 140f, 135.78f, 139.09f, 137.44f, 137.43f)
+                curveTo(139.09f, 135.78f, 140f, 133.58f, 140f, 131.24f)
+                curveTo(140f, 128.91f, 139.09f, 126.71f, 137.44f, 125.06f)
+                lineTo(82.37f, 69.96f)
+                lineTo(137.44f, 14.86f)
+                curveTo(139.09f, 13.21f, 140f, 11.01f, 140f, 8.67f)
+                close()
+            }
+        }.build()
+
+        return _Cross!!
+    }
+
+@Suppress("ObjectPropertyName")
+private var _Cross: ImageVector? = null
