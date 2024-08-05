fa415d042273788759aa96affb3cdea62049d140
Stepan <stepan.bodnar43@gmail.com>
2024-08-05 15:36:11 +0700

    feat: Change App Icon

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -3,7 +3,10 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import localtictactoe.composeapp.generated.resources.Res
+import localtictactoe.composeapp.generated.resources.logo
 import navigation.RootComponent
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
     val root = remember {
@@ -11,8 +14,9 @@ fun main() = application {
     }
     Window(
         //state = WindowState(width = 1400.dp, height = 800.dp),
+        icon = painterResource(Res.drawable.logo),
         onCloseRequest = ::exitApplication,
-        title = "LocalTicTacToe",
+        title = "Крестики-Нолики",
 
     ) {
         App(root)
