b29b2c50fdfa07dde727e5e392cf1501eb447d5f
Stepan <stepan.bodnar43@gmail.com>
2024-08-09 02:45:28 +0700

    feat: Add unfocus action

--- composeApp/src/desktopMain/kotlin/ui/HomeScreen.kt
+++ composeApp/src/desktopMain/kotlin/ui/HomeScreen.kt
@@ -4,6 +4,8 @@ package ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -42,6 +44,13 @@ fun HomeScreen(component: HomeScreenComponent){
             .fillMaxSize()
             .background(
                 color = theme.background
+            )
+            .clickable(
+                onClick = {
+                    focus.clearFocus()
+                },
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
             ),
         contentAlignment = Alignment.Center
     ) {
